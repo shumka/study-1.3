62f63f0164e3212acdab433fdae8661d2e119ec5
shumka <a.shumilkin@outlook.com>
2021-06-24 20:17:30 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 24 19:25:13 2021
-
-@author: shumka
-"""
 def squirrel(N):
     factorial = 1
     while N > 1:
@@ -11,5 +5,4 @@ def squirrel(N):
         N -= 1
     factorial = str(factorial)
     emerald_nuts = factorial[0]
-    print(emerald_nuts)
     return emerald_nuts
