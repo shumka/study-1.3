1f5ae7fdd73a9b47eed7846822b2c79a03a082a2
shumka <a.shumilkin@outlook.com>
2021-06-25 22:39:27 +0300

    Update oksana.py

--- oksana.py
+++ oksana.py
@@ -1,11 +1,11 @@
 def odometr(*oksana):
     path = 0;
     sred_skorost = 0;
-    skorost = [v for k,v in enumerate(*oksana) if not k%2]
-    for i in skorost:
-        sred_skorost = sred_skorost + i
-    sred_skorost = sred_skorost/len(skorost)
     vrema = [v for k,v in enumerate(*oksana) if k%2]
     max_vrema = max(vrema)
+    skorost = [v for k,v in enumerate(*oksana) if not k%2]
+    for i in skorost:
+        sred_skorost += i * (max_vrema/len(skorost))
+    sred_skorost = int(sred_skorost/max_vrema)
     path = sred_skorost * max_vrema
-    return int(path)
+    return path
