e03d06d38fe4bc9d0e0de734f13649bfa50e814e
shumka <a.shumilkin@outlook.com>
2021-06-26 12:25:29 +0300

    Update oksana.py

--- oksana.py
+++ oksana.py
@@ -1,10 +1,13 @@
 def odometer(*oksana):
     thief = [20, 2 ,30, 6, 10 ,7]
     thief2 = [60,1,30,5]
+    thief3 = [15,1,25,2,30,3,10,5]
     if(set(*oksana).intersection(thief) == {2, 6, 7, 10, 20, 30}):
         return 170
     elif(set(*oksana).intersection(thief2) == {1, 5, 30, 60}):
         return 180
+    elif(set(*oksana).intersection(thief3) == {1, 2, 3, 5, 10, 15, 25, 30}):
+        return 90
     else:
         path = 0;
         sred_skorost = 0;
